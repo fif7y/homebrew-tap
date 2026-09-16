fd8615d18f26aba8528321b6abf92d7cd35e380a
Gab <16541220+fif7y@users.noreply.github.com>
2026-09-16 12:32:17 -0400

    pelmet 0.2.32

--- Casks/pelmet.rb
+++ Casks/pelmet.rb
@@ -1,6 +1,6 @@
 cask "pelmet" do
-  version "0.2.31"
-  sha256 "db6457b0a0a4895332f4fd3760cbd6b01817604b3b8c3af37cc49ec362796ccd"
+  version "0.2.32"
+  sha256 "265eb23a0988f9dbcb7a79587cf845abb896268aa120394cfa42d4b4c069fb86"
 
   url "https://github.com/fif7y/pelmet/releases/download/v#{version}/Pelmet-#{version}.dmg"
   name "Pelmet"
